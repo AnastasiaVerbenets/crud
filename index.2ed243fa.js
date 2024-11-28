8991ff74a5efe258f5a02ffa7c6dddf96eda3644
AnastasiaVerbenets <anastasiaverbenets@gmail.com>
2024-11-28 16:07:44 +0000

    Deploying to gh-pages from @ AnastasiaVerbenets/crud@8aefb0c8c61fd1bb2b325c24faefee21b3ac51f5 🚀

--- index.2ed243fa.js
+++
@@ -1,2 +0,0 @@
-fetch("http://localhost:4040/students").then((o=>o.json())).then((o=>console.log(o))).catch((o=>console.log(o)));
-//# sourceMappingURL=index.2ed243fa.js.map
--- index.7c109d94.js
+++
@@ -1,2 +0,0 @@
-fetch("http://localhost:4040/students").then((function(n){return n.json()})).then((function(n){return console.log(n)})).catch((function(n){return console.log(n)}));
-//# sourceMappingURL=index.7c109d94.js.map
